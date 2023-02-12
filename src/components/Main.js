70035fbaba6402e4b597aa123e8c879da9255415
Vivek Kumar <99972016+Vivek-27@users.noreply.github.com>
2023-02-12 21:00:25 +0530

    shadow

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,111 +1,114 @@
-import React from 'react'
-import './Main.css'
-import { useState } from 'react'
-import axios from 'axios'
+import React from 'react';
+import './Main.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Main = () => {
+  const [location, setLocation] = useState({
+    location: ''
+  });
 
-const [location, setLocation] = useState({
-    location: "",
-})
+  const [city, setCity] = useState({
+    name: 'Enter city name📍',
+    temp: '',
+    wind: '',
+    con_text: ''
+  });
 
+  const [icon, setIcon] = useState({
+    icon: ''
+  });
 
-const [city, setCity ] = useState({
-  name:"Enter city name📍",
-  temp:"",
-  wind:"",
-  con_text: "",
-})
+  const [loading, setLoading] = useState(false);
 
-const [icon, setIcon ] = useState({
-  icon: "",
-})
-
-const [loading, setLoading] = useState(false)
-
-
-const handelChange = e => {
-    const {name, value} = e.target
+  const handelChange = (e) => {
+    const { name, value } = e.target;
     setLocation({
-        [name]: value,
-    })
-}
+      [name]: value
+    });
+  };
 
+  const search = async (e) => {
+    setLoading(true);
 
-const search = async e => {
-  setLoading(true)
-  
-        const {data} = await axios.get(
-          `http://api.weatherapi.com/v1/current.json?key=cd25bc2669e34c48a6f133357230302&q=${location.location}&aqi=yes`
+    const { data } = await axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=cd25bc2669e34c48a6f133357230302&q=${location.location}&aqi=yes`
       )
-          setCity({
-            name: data.location.name + `📍`,
-            temp: data.current.temp_c + `° C`,
-            wind: `Wind Speed: ` + data.current.wind_kph + `Kph`,
-            con_text: data.current.condition.text,
-          })
-          setIcon({
-            icon: data.current.condition.icon,
-          })
-  setLoading(false)
-}
+      .catch((error) => {
+        if (error) alert('Enter valid location !');
+        setLoading(false);
+      });
+    setCity({
+      name: data.location.name + `📍`,
+      temp: data.current.temp_c + `° C`,
+      wind: `Wind Speed: ` + data.current.wind_kph + `Kph`,
+      con_text: data.current.condition.text
+    });
+    setIcon({
+      icon: data.current.condition.icon
+    });
+    setLoading(false);
+  };
 
+  const spinner = (
+    <>
+      <div className="spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
+    </>
+  );
 
-
-const spinner=(
-  <>
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
-    </div>
-  </>
-)
-
-
-const render=(
-  <>
-    <div className='wrapper'>
+  const render = (
+    <>
+      <div className="wrapper">
         <h1>Weather Today</h1>
-            <div>
-               <img alt='' src={icon.icon} />
-              <h4>{city.con_text}</h4>
-            </div>
-            <div className='detail'>
-              <h2>{city.name}</h2>
-              <h3>{city.temp}</h3>
-              <h4>{city.wind}</h4>
-            </div>
-            <input 
-              type='text' 
-              name='location' 
-              value={location.location} 
-              placeholder='Location📍' 
-              onChange={handelChange} 
-              onKeyDown={(e)=>{
-                if(e.key === "Enter"){
-                  search()
-                }
-              }}
-            />
-            <button className='button btn' onClick={()=>search()}>Search</button>
-      </div> 
-  </>
-
-)
+        <div>
+          <img alt="" src={icon.icon} />
+          <h4>{city.con_text}</h4>
+        </div>
+        <div className="detail">
+          <h2>{city.name}</h2>
+          <h3>{city.temp}</h3>
+          <h4>{city.wind}</h4>
+        </div>
+        <input
+          type="text"
+          name="location"
+          value={location.location}
+          placeholder="Location📍"
+          onChange={handelChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              search();
+            }
+          }}
+        />
+        <button className="button btn" onClick={() => search()}>
+          Search
+        </button>
+      </div>
+    </>
+  );
 
   return (
-      // <div className='container'>
-      //   {loading ? <p style={{display:'flex', justifyContent:'center', alignItems:'center' }}>loading</p> : render}
-      // </div>
+    // <div className='container'>
+    //   {loading ? <p style={{display:'flex', justifyContent:'center', alignItems:'center' }}>loading</p> : render}
+    // </div>
 
-      <div className='container'>
+    <div className="container">
       {/* <div className='ef ef1'></div> */}
-        
-        {loading ? spinner : render}
+
+      {loading ? spinner : render}
 
       {/* <div className='ef ef2'></div> */}
-      <img className='bac' alt='' src={`https://source.unsplash.com/random/1200x750/?+${location.location}`} />
+      <img
+        className="bac"
+        alt=""
+        src={`https://source.unsplash.com/random/1200x750/?+${location.location}`}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
